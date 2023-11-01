82197942b47a31cffa127a76b5f75748c9f909a9
Thomas Lion <t.lion@ateme.com>
2023-11-01 19:04:19 +0100

    [FE] Add post fetch

--- backend/public/js/custom.js
+++ backend/public/js/custom.js
@@ -2,25 +2,33 @@ const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'num
 const dt = new Date().toLocaleDateString(undefined, dateOptions)
 document.getElementById('daily-date').innerHTML=dt
 
-async function getEvents() {
-    const requestOptions = {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-        },
-        mode: "cors",
-        cache: "default",
-      }
-    
-    const myRequest = new Request("api/events", requestOptions)
-    try {
-        const response = await fetch(myRequest, requestOptions)
-        const events = await response.json()
-        console.log(events.data)
-    } catch (error) {
-        console.error(error)
-    }
+async function httpClient(route, method, body = null) {
+  const requestOptions = {
+    method,
+    headers: {
+      Accept: "application/json",
+    },
+    mode: "cors",
+    cache: "no-cache",
+    body
+  }
+  const myRequest = new Request(route, requestOptions)
+  try {
+      const response = await fetch(myRequest, requestOptions)
+      const events = await response.json()
+      console.log(events.data)
+  } catch (error) {
+      console.error(error)
+  }
 }
 
+async function getEvents() {
+  await httpClient("api/events", "GET")
+}
+
+async function postEvent(event) {
+  await httpClient("api/events", "POST", JSON.stringify(event))
+}
+
+// Get events in the app starting
 getEvents()
-  
