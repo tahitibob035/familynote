9c22cd6ef15607108f2d7dabd9c70d6fa2a5d293
Thomas Lion <t.lion@ateme.com>
2023-11-05 17:40:41 +0100

    [SQL] Update data script.sql

--- backend/scripts/data.sql
+++ backend/scripts/data.sql
@@ -2,6 +2,6 @@ SET NAMES UTF8;
 
 USE familynote;
 INSERT INTO events ( label, datetime, location, description) VALUES
-  ('Gabriel', '2015-11-28 00:00:00', 'Rennes', 'Hôpital Sud'),
-  ('Matiss', '2008-12-31 00:00:00', 'Caen', 'Hôpital clémenceau'),
-  ('Thomas', '1981-07-23 00:00:00', 'Flers', null);
+  ('Anniversaire Gabriel', '2015-11-28 00:00:00', 'Rennes', 'Hôpital Sud'),
+  ('Anniversaire Matiss', '2008-12-31 00:00:00', 'Caen', 'Hôpital clémenceau'),
+  ('Anniversaire Thomas', '1981-07-23 00:00:00', 'Flers', null);
