f518e308c3fb1ff4bb3fe086453aadbc0bda6973
Thomas Lion <t.lion@ateme.com>
2023-11-05 10:24:43 +0100

    [FRONT] add search and dynamic event list

--- backend/public/js/custom.js
+++ backend/public/js/custom.js
@@ -1,6 +1,8 @@
 const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
 const dt = new Date().toLocaleDateString(undefined, dateOptions)
 document.getElementById('daily-date').innerHTML=dt
+let events = []
+
 
 async function httpClient(route, method, body = null) {
   const requestOptions = {
@@ -15,15 +17,46 @@ async function httpClient(route, method, body = null) {
   const myRequest = new Request(route, requestOptions)
   try {
       const response = await fetch(myRequest, requestOptions)
-      const events = await response.json()
-      console.log(events.data)
+      return await response.json()
   } catch (error) {
       console.error(error)
+      return null
   }
 }
 
 async function getEvents() {
-  await httpClient("api/events", "GET")
+  events = await httpClient("api/events", "GET")
+  displayEventsList(events)
+}
+
+function onSearch(evt) {
+  const filterString = evt.target.value
+  displayEventsList(events.filter(event => {
+    for (const key in event) {
+      if (String(event[key]).toLowerCase().includes(String(filterString).toLowerCase())) {
+        return true
+      }
+    }
+  }))
+}
+
+function displayEventsList(events = []) {
+  const columnsToHide = ['id', 'file']
+  const eventsElement = document.getElementById('events-list')
+  eventsElement.textContent = ''
+  events.forEach(element => {
+    const eventElement = document.createElement('tr')
+    Object.keys(element).filter(name => !columnsToHide.includes(name))
+      .forEach(name => {
+        const columnElement = document.createElement('td')
+        if (element[name]) {
+          const columnContent = document.createTextNode(element[name])
+          columnElement.appendChild(columnContent)
+        }
+        eventElement.appendChild(columnElement)
+    })
+    eventsElement.appendChild(eventElement)
+  })
 }
 
 async function postEvent(event) {
