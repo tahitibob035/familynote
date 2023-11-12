a0424d38155e3c6f004ef7c6edc85ee47a17e996
Thomas Lion <t.lion@ateme.com>
2023-11-12 14:03:45 +0100

    Fix typo

--- backend/scripts/data.sql
+++ backend/scripts/data.sql
@@ -2,6 +2,6 @@ SET NAMES UTF8;
 
 USE familynote;
 INSERT INTO events ( label, datetime, location, description) VALUES
-  ('Anniversaire Gabriel', '2015-11-28 00:00:00', 'Rennes', 'Hôpital Sud'),
-  ('Anniversaire Matiss', '2008-12-31 00:00:00', 'Caen', 'Hôpital clémenceau'),
-  ('Anniversaire Thomas', '1981-07-23 00:00:00', 'Flers', null);
+  ('Naissance de Gabriel', '2015-11-28 00:00:00', 'Rennes', 'Hôpital Sud'),
+  ('Naissance de Matiss', '2008-12-31 00:00:00', 'Caen', 'Hôpital clémenceau'),
+  ('Naissance de Thomas', '1981-07-23 00:00:00', 'Flers', null);
